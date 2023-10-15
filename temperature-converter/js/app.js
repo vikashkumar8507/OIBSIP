18c70b5cc42997549e232ac23e12d50d3ac8b5bd
Vikash Kumar <72180033+vikashkumar8507@users.noreply.github.com>
2023-10-16 00:31:22 +0530

    Add files via upload

---
+++ temperature-converter/js/app.js
@@ -0,0 +1,58 @@
+// script.js
+const celsiusField = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusField.innerHTML = "";
+});
+
+if(degree.value === ""){
+  convertBtn.setAttribute("disabled","");
+  setTimeout(() => {
+    convertBtn.removeAttribute('disabled');
+  }, 4000);
+}
+
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+  convertBtn.innerHTML = "<span class='icon'><i class='fa fa-spinner fa-spin'></i> Converting...</span>";
+  setTimeout(() => {
+    convertBtn.innerHTML ="<span>Convert</span>"
+  }, 1000);
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+  
+  setTimeout( () => {
+    if (tempType.value === "fahrenheit") {
+      const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+      celsiusField.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
+    } else if (tempType.value === "kelvin") {
+      const KelvinToCelsius = inputValue - 273.15;
+      celsiusField.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;c`;
+    }
+  }, 1200)
+}
+
+
+//current time 
+
+function updateDateTime() {
+  // create a new `Date` object
+  const now = new Date();
+
+  // get the current date and time as a string
+  const currentDateTime = now.toLocaleString();
+
+  // update the `textContent` property of the `span` element with the `id` of `datetime`
+  document.querySelector('.time').textContent = currentDateTime;
+}
+
+// call the `updateDateTime` function every second
+setInterval(updateDateTime, 1000);
